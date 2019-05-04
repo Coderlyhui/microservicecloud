a66437d9ecef8de1d0fe095ebb7da397347eecdd
lyh_pro <hello_pro@lyh.com>
2019-05-04 15:45:25 +0800

    处理config模块依赖导入错误（spring-cloud-config-server）

--- microservicecloud-config-3344/src/main/java/com/lyh/Config_3344_StartSpringCloud_App.java
+++ microservicecloud-config-3344/src/main/java/com/lyh/Config_3344_StartSpringCloud_App.java
@@ -2,7 +2,7 @@ package com.lyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
